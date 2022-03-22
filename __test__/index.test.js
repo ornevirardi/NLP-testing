19a6d6dfec4398aff09448be38577cdfa0085849
Orne Virardi <73016255+ornevirardi@users.noreply.github.com>
2022-03-22 11:17:44 -0700

    Update index.test.js

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,9 +1,9 @@
-import {baseURL} from "../src/server/index";
+import { baseURL } from '../src/server/index.js';
 
 const basesURL = require('../src/server/index.js');
 describe('The base URL is complete', () => {
     test('base url is complete', () => {
-      expect(basesURL).toMatch("https://api.meaningcloud.com/sentiment-2.1?");
+      expect(basesURL).toMatch("https://api.meaningcloud.com/sentiment-2.1?key=");
     });
   });
 
