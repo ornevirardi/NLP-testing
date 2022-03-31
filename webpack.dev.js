e92af2462f52a9be9b830de1c3d7078c013c1436
Orne Virardi <73016255+ornevirardi@users.noreply.github.com>
2022-03-31 18:09:02 -0400

    Add files via upload

---
+++ webpack.dev.js
@@ -0,0 +1,56 @@
+const path = require ('path');
+const webpack = require ('webpack');
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+    entry: './src/client/index.js',
+    output: {
+        libraryTarget: 'var',
+        library: 'Client',
+        clean: true,
+        path: path.join(__dirname, 'dist'),
+        filename: 'main.js',
+        publicPath: '/'
+    },
+    mode: 'development',
+    devtool: 'source-map',
+    stats: 'verbose',
+    devServer: {
+        compress: true,
+        port: 8080,
+        historyApiFallback: true,
+      },
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader' ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        }),
+        new CleanWebpackPlugin({
+            // Simulate the removal of files
+            dry: true,
+            // Write Logs to Console
+            verbose: true,
+            // Automatically remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
+        }),
+        // new WorkboxPlugin.GenerateSW()
+    ],
+    optimization:{
+        minimize: true,
+    },
+}
